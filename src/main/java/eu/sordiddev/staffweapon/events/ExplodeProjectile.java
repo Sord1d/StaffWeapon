48f871e8f7f4b069d86473c5fd6c44963e194220
sord1d <andyahl95@sordiddev.eu>
2022-01-30 23:45:57 +0100

    basic damage system added

--- src/main/java/eu/sordiddev/staffweapon/events/ExplodeProjectile.java
+++ src/main/java/eu/sordiddev/staffweapon/events/ExplodeProjectile.java
@@ -2,12 +2,15 @@ package eu.sordiddev.staffweapon.events;
 
 import com.destroystokyo.paper.ParticleBuilder;
 import com.destroystokyo.paper.event.entity.ProjectileCollideEvent;
+import eu.sordiddev.staffweapon.Staffweapon;
 import org.bukkit.*;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
@@ -25,22 +28,33 @@ public class ExplodeProjectile implements Listener {
             //the projectile should be called StaffWeapon - any other collision can be ignored
             if (e.getEntity().getCustomName().contains("StaffWeapon")){
 
-                //patricle stuff
+                //particle stuff
                 ParticleBuilder explosion = new ParticleBuilder(Particle.SMOKE_NORMAL);
                 ParticleBuilder flame = new ParticleBuilder(Particle.FLAME);
                 explosion.allPlayers().location(e.getEntity().getLocation()).count(50).spawn();
                 flame.allPlayers().location(e.getEntity().getLocation()).count(20).spawn();
                 e.getEntity().remove();
 
-               //TODO DEAL DAMAGE
-
-
-                Player player = (Player) e.getEntity().getShooter();
-                player.sendMessage("Boom!");
-
-
             }
 
+        return;
+
+    }
+
+    @EventHandler
+    public void Damage(EntityDamageByEntityEvent e){
+        if (e.getDamager().getName().contains("StaffWeapon"))  {
+
+            //TODO DAMAGE CONFIG FILE
+
+            Snowball projectile = (Snowball) e.getDamager();
+            Player player = (Player) projectile.getShooter();
+            e.setDamage(15);
+
+            player.sendMessage("Boom!"); //Todo remove
+
+        }
+        return;
     }
 
 
